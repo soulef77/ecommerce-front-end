277a2904e75f464a2e85ea7892a3891d27b8bae6
soulef77 <saoud.soulef@laposte.net>
2026-01-06 23:28:58 +0100

    commit 1 frontend pour le deploiement Railway

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // @ts-check
 import eslint from '@eslint/js';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
@@ -33,3 +34,23 @@ export default tseslint.config(
     },
   },
 );
+=======
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
+
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+  ]),
+]);
+
+export default eslintConfig;
+>>>>>>> c038fd2 (Initial commit from Create Next App)
